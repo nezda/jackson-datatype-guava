e24c3592377a4dd2605e5db6e631adad44cb7c09
Luke Nezda <lnezda@users.sourceforge.net>
2012-06-26 23:04:33 -0500

    - unsophisticated copy/port of MapSerialzier leveraging existing functional code to get types working (deserialization already seems to work?); - unit tests showing issue;

--- src/main/java/com/fasterxml/jackson/datatype/guava/ser/MultimapSerializer.java
+++ src/main/java/com/fasterxml/jackson/datatype/guava/ser/MultimapSerializer.java
@@ -8,14 +8,18 @@ import com.fasterxml.jackson.core.*;
 
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
+import com.fasterxml.jackson.databind.ser.ContainerSerializer;
 import com.fasterxml.jackson.databind.ser.ContextualSerializer;
+import com.fasterxml.jackson.databind.ser.impl.PropertySerializerMap;
 import com.fasterxml.jackson.databind.type.MapLikeType;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class MultimapSerializer
-    extends JsonSerializer<Multimap<?, ?>>
+		extends ContainerSerializer<Multimap<?,?>>
     implements ContextualSerializer
 {
     private final MapLikeType type;
@@ -23,6 +27,11 @@ public class MultimapSerializer
     private final JsonSerializer<Object> keySerializer;
     private final TypeSerializer valueTypeSerializer;
     private final JsonSerializer<Object> valueSerializer;
+		/**
+     * If value type can not be statically determined, mapping from
+     * runtime value types to serializers are stored in this object.
+     */
+    protected PropertySerializerMap _dynamicValueSerializers;
 
     public MultimapSerializer(SerializationConfig config,
             MapLikeType type,
@@ -31,11 +40,13 @@ public class MultimapSerializer
             TypeSerializer valueTypeSerializer,
             JsonSerializer<Object> valueSerializer)
     {
+				super(Multimap.class, false);
         this.type = type;
         this.property = null;
         this.keySerializer = keySerializer;
         this.valueTypeSerializer = valueTypeSerializer;
         this.valueSerializer = valueSerializer;
+				this._dynamicValueSerializers = PropertySerializerMap.emptyMap();
     }
 
     @SuppressWarnings("unchecked")
@@ -43,14 +54,27 @@ public class MultimapSerializer
                 JsonSerializer<?> keySerializer,
                 TypeSerializer valueTypeSerializer, JsonSerializer<?> valueSerializer)
     {
+				super(Multimap.class, false);
         this.type = src.type;
         this.property = property;
         this.keySerializer = (JsonSerializer<Object>) keySerializer;
         this.valueTypeSerializer = valueTypeSerializer;
         this.valueSerializer = (JsonSerializer<Object>) valueSerializer;
+				this._dynamicValueSerializers = src._dynamicValueSerializers;
     }
-            
-    
+
+		protected MultimapSerializer(MultimapSerializer src, TypeSerializer vts)
+    {
+        super(Multimap.class, false);
+				this.type = src.type;
+//        _valueTypeIsStatic = src._valueTypeIsStatic;
+        valueTypeSerializer = vts;
+        keySerializer = src.keySerializer;
+        valueSerializer = src.valueSerializer;
+        _dynamicValueSerializers = src._dynamicValueSerializers;
+        property = src.property;
+    }
+
     protected MultimapSerializer withResolved(BeanProperty property,
             JsonSerializer<?> keySerializer,
             TypeSerializer valueTypeSerializer, JsonSerializer<?> valueSerializer)
@@ -58,13 +82,14 @@ public class MultimapSerializer
         return new MultimapSerializer(this, property, keySerializer,
                 valueTypeSerializer, valueSerializer);
     }
-    
+
     /*
     /**********************************************************
     /* Post-processing (contextualization)
     /**********************************************************
      */
 
+		@Override
     public JsonSerializer<?> createContextual(SerializerProvider provider,
             BeanProperty property) throws JsonMappingException
     {
@@ -96,7 +121,7 @@ public class MultimapSerializer
     /* JsonSerializer implementation
     /**********************************************************
      */
-    
+
     @Override
     public void serialize(Multimap<?, ?> value, JsonGenerator jgen, SerializerProvider provider)
             throws IOException, JsonProcessingException
@@ -104,7 +129,7 @@ public class MultimapSerializer
         jgen.writeStartObject();
         if (!value.isEmpty()) {
             serializeFields(value, jgen, provider);
-        }        
+        }
         jgen.writeEndObject();
     }
 
@@ -118,26 +143,150 @@ public class MultimapSerializer
         typeSer.writeTypeSuffixForObject(value, jgen);
     }
 
-    private final void serializeFields(Multimap<?, ?> value, JsonGenerator jgen, SerializerProvider provider)
+    private void serializeFields(Multimap<?, ?> value, JsonGenerator jgen, SerializerProvider provider)
             throws IOException, JsonProcessingException
     {
+				// If value type needs polymorphic type handling, some more work needed:
+        if (valueTypeSerializer != null) {
+            serializeTypedFields(value, jgen, provider);
+            return;
+        }
+				PropertySerializerMap serializers = _dynamicValueSerializers;
         for (Entry<?, ? extends Collection<?>> e : value.asMap().entrySet()) {
+						final Object keyElem = e.getKey();
             if (keySerializer != null) {
-                keySerializer.serialize(e.getKey(), jgen, provider);
+                keySerializer.serialize(keyElem, jgen, provider);
             } else {
                 provider.findKeySerializer(provider.constructType(String.class), property)
-                    .serialize(e.getKey(), jgen, provider);
+                    .serialize(keyElem, jgen, provider);
             }
             if (valueSerializer != null) {
+								jgen.writeStartArray();
                 // note: value is a List, but generic type is for contents... so:
-                jgen.writeStartArray();
                 for (Object vv : e.getValue()) {
                     valueSerializer.serialize(vv, jgen, provider);
                 }
-                jgen.writeEndArray();
+								jgen.writeEndArray();
             } else {
-                provider.defaultSerializeValue(Lists.newArrayList(e.getValue()), jgen);
+								Object valueElem = e.getValue();
+								Class<?> cc = valueElem.getClass();
+                JsonSerializer<Object> serializer = serializers.serializerFor(cc);
+                if (serializer == null) {
+                    if (type.getContentType().hasGenericTypes()) {
+                        serializer = _findAndAddDynamic(serializers,
+                                provider.constructSpecializedType(type.getContentType(), cc), provider);
+                    } else {
+                        serializer = _findAndAddDynamic(serializers, cc, provider);
+                    }
+                    serializers = _dynamicValueSerializers;
+                }
+                try {
+                    serializer.serialize(valueElem, jgen, provider);
+                } catch (Exception ex) {
+                    // [JACKSON-55] Need to add reference information
+                    String keyDesc = ""+keyElem;
+                    wrapAndThrow(provider, ex, value, keyDesc);
+                }
             }
         }
+    }
+
+		protected void serializeTypedFields(Multimap<?,?> value, JsonGenerator jgen, SerializerProvider provider)
+        throws IOException, JsonGenerationException
+    {
+        JsonSerializer<Object> prevValueSerializer = null;
+        Class<?> prevValueClass = null;
+        final boolean skipNulls = !provider.isEnabled(SerializationFeature.WRITE_NULL_MAP_VALUES);
+
+				for (Entry<?, ? extends Collection<?>> entry : value.asMap().entrySet()) {
+            // First, serialize key
+            Collection<?> valuesElem = entry.getValue();
+            Object keyElem = entry.getKey();
+            if (keyElem == null) {
+                provider.findNullKeySerializer(type.getKeyType(), property).serialize(null, jgen, provider);
+            } else {
+                // [JACKSON-314] also may need to skip entries with null values
+                if (skipNulls && valuesElem == null) continue;
+                keySerializer.serialize(keyElem, jgen, provider);
+            }
+
+            // And then values
+						jgen.writeStartArray();
+						for (Object valueElem : valuesElem) {
+							if (valueElem == null) {
+									provider.defaultSerializeNull(jgen);
+							} else {
+									Class<?> cc = valueElem.getClass();
+									JsonSerializer<Object> currSerializer;
+									if (cc == prevValueClass) {
+											currSerializer = prevValueSerializer;
+									} else {
+											currSerializer = provider.findValueSerializer(cc, property);
+											prevValueSerializer = currSerializer;
+											prevValueClass = cc;
+									}
+									try {
+											currSerializer.serializeWithType(valueElem, jgen, provider, valueTypeSerializer);
+									} catch (Exception e) {
+											// [JACKSON-55] Need to add reference information
+											String keyDesc = ""+keyElem;
+											wrapAndThrow(provider, e, value, keyDesc);
+									}
+							}
+						}
+						jgen.writeEndArray();
+        }
+    }
+
+		@Override
+    public JavaType getContentType() {
+        return type.getContentType();
+    }
+
+    @Override
+    public JsonSerializer<?> getContentSerializer() {
+        return valueSerializer;
+    }
+
+    @Override
+    public boolean isEmpty(Multimap<?,?> value) {
+        return (value == null) || value.isEmpty();
+    }
+
+    @Override
+    public boolean hasSingleElement(Multimap<?,?> value) {
+        return (value.size() == 1);
+    }
+
+		@Override
+		protected MultimapSerializer _withValueTypeSerializer(TypeSerializer vts) {
+			return new MultimapSerializer(this, vts);
+		}
+
+		/*
+    /**********************************************************
+    /* Internal helper methods
+    /**********************************************************
+     */
+
+    protected final JsonSerializer<Object> _findAndAddDynamic(PropertySerializerMap map,
+            Class<?> type, SerializerProvider provider) throws JsonMappingException
+    {
+        PropertySerializerMap.SerializerAndMapResult result = map.findAndAddSerializer(type, provider, property);
+        // did we get a new map of serializers? If so, start using it
+        if (map != result.map) {
+            _dynamicValueSerializers = result.map;
+        }
+        return result.serializer;
+    }
+
+    protected final JsonSerializer<Object> _findAndAddDynamic(PropertySerializerMap map,
+            JavaType type, SerializerProvider provider) throws JsonMappingException
+    {
+        PropertySerializerMap.SerializerAndMapResult result = map.findAndAddSerializer(type, provider, property);
+        if (map != result.map) {
+            _dynamicValueSerializers = result.map;
+        }
+        return result.serializer;
     }
 }
--- src/test/java/com/fasterxml/jackson/datatype/guava/TestMultimaps.java
+++ src/test/java/com/fasterxml/jackson/datatype/guava/TestMultimaps.java
@@ -1,18 +1,28 @@
 package com.fasterxml.jackson.datatype.guava;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Objects;
 
 import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.ImmutableMultiset;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.LinkedListMultimap;
 import com.google.common.collect.TreeMultimap;
 import static com.google.common.collect.TreeMultimap.create;
+import java.util.Collection;
 
 /**
  * Unit tests to verify handling of various {@link Multimap}s.
@@ -21,75 +31,188 @@ import static com.google.common.collect.TreeMultimap.create;
  */
 public class TestMultimaps extends BaseTest
 {
-    private final ObjectMapper MAPPER =  mapperWithModule();
+	private final ObjectMapper MAPPER = mapperWithModule();
 
-    public void testMultimap() throws Exception
-    {
-        _testMultimap(TreeMultimap.create(), true,
-                "{\"false\":[false],\"maybe\":[false,true],\"true\":[true]}");
-        _testMultimap(LinkedListMultimap.create(), false,
-                "{\"true\":[true],\"false\":[false],\"maybe\":[true,false]}");
-        _testMultimap(LinkedHashMultimap.create(), false, null);
-    }
-    
-    private void _testMultimap(Multimap<?,?> map0, boolean fullyOrdered, String EXPECTED) throws Exception
-    {
-        @SuppressWarnings("unchecked")
-        Multimap<String,Boolean> map = (Multimap<String,Boolean>) map0;
-        map.put("true", Boolean.TRUE);
-        map.put("false", Boolean.FALSE);
-        map.put("maybe", Boolean.TRUE);
-        map.put("maybe", Boolean.FALSE);
+	public void testMultimap() throws Exception
+	{
+		_testMultimap(TreeMultimap.create(), true,
+			"{\"false\":[false],\"maybe\":[false,true],\"true\":[true]}");
+		_testMultimap(LinkedListMultimap.create(), false,
+			"{\"true\":[true],\"false\":[false],\"maybe\":[true,false]}");
+		_testMultimap(LinkedHashMultimap.create(), false, null);
+	}
 
-        // Test that typed writes work
-        if (EXPECTED != null) {
-            String json =  MAPPER.writerWithType(new TypeReference<Multimap<String, Boolean>>() {}).writeValueAsString(map);
-            assertEquals(EXPECTED, json);
-        }
+	private void _testMultimap(Multimap<?, ?> map0, boolean fullyOrdered, String EXPECTED) throws Exception
+	{
+		@SuppressWarnings("unchecked")
+		Multimap<String, Boolean> map = (Multimap<String, Boolean>) map0;
+		map.put("true", Boolean.TRUE);
+		map.put("false", Boolean.FALSE);
+		map.put("maybe", Boolean.TRUE);
+		map.put("maybe", Boolean.FALSE);
 
-        // And untyped too
-        String serializedForm = MAPPER.writeValueAsString(map);
+		// Test that typed writes work
+		if (EXPECTED != null) {
+			String json = MAPPER.writerWithType(new TypeReference<Multimap<String, Boolean>>() {}).writeValueAsString(map);
+			assertEquals(EXPECTED, json);
+		}
 
-        if (EXPECTED != null) {
-            assertEquals(EXPECTED, serializedForm);
-        }
+		// And untyped too
+		String serializedForm = MAPPER.writeValueAsString(map);
 
-        // these seem to be order-sensitive as well, so only use for ordered-maps
-        if (fullyOrdered) {
-            assertEquals(map, MAPPER.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<TreeMultimap<String, Boolean>>() {}));
-            assertEquals(map, create(MAPPER.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<Multimap<String, Boolean>>() {})));
-            assertEquals(map, create(MAPPER.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<HashMultimap<String, Boolean>>() {})));
-            assertEquals(map, create(MAPPER.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<ImmutableMultimap<String, Boolean>>() {})));
-        }
-    }
+		if (EXPECTED != null) {
+			assertEquals(EXPECTED, serializedForm);
+		}
 
-    public void testMultimapIssue3() throws Exception
-    {
-        Multimap<String, String> m1 = TreeMultimap.create();
-        m1.put("foo", "bar");
-        m1.put("foo", "baz");
-        m1.put("qux", "quux");
-        ObjectMapper o = MAPPER;
-        
-        String t1 = o.writerWithType(new TypeReference<TreeMultimap<String, String>>(){}).writeValueAsString(m1);
-        Map<?,?> javaMap = o.readValue(t1, Map.class);
-        assertEquals(2, javaMap.size());
-        
-        String t2 = o.writerWithType(new TypeReference<Multimap<String, String>>(){}).writeValueAsString(m1);
-        javaMap = o.readValue(t2, Map.class);
-        assertEquals(2, javaMap.size());
-        
-        TreeMultimap<String, String> m2 = TreeMultimap.create();
-        m2.put("foo", "bar");
-        m2.put("foo", "baz");
-        m2.put("qux", "quux");
-        
-        String t3 = o.writerWithType(new TypeReference<TreeMultimap<String, String>>(){}).writeValueAsString(m2);
-        javaMap = o.readValue(t3, Map.class);
-        assertEquals(2, javaMap.size());
-   
-        String t4 = o.writerWithType(new TypeReference<Multimap<String, String>>(){}).writeValueAsString(m2);
-        javaMap = o.readValue(t4, Map.class);
-        assertEquals(2, javaMap.size());
-    }
+		// these seem to be order-sensitive as well, so only use for ordered-maps
+		if (fullyOrdered) {
+			assertEquals(map, MAPPER.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<TreeMultimap<String, Boolean>>() {}));
+			assertEquals(map, create(MAPPER.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<Multimap<String, Boolean>>() {})));
+			assertEquals(map, create(MAPPER.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<HashMultimap<String, Boolean>>() {})));
+			assertEquals(map, create(MAPPER.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<ImmutableMultimap<String, Boolean>>() {})));
+		}
+	}
+
+	public void testMultimapIssue3() throws Exception
+	{
+		Multimap<String, String> m1 = TreeMultimap.create();
+		m1.put("foo", "bar");
+		m1.put("foo", "baz");
+		m1.put("qux", "quux");
+		ObjectMapper o = MAPPER;
+
+		String t1 = o.writerWithType(new TypeReference<TreeMultimap<String, String>>() {}).writeValueAsString(m1);
+		Map<?, ?> javaMap = o.readValue(t1, Map.class);
+		assertEquals(2, javaMap.size());
+
+		String t2 = o.writerWithType(new TypeReference<Multimap<String, String>>() {}).writeValueAsString(m1);
+		javaMap = o.readValue(t2, Map.class);
+		assertEquals(2, javaMap.size());
+
+		TreeMultimap<String, String> m2 = TreeMultimap.create();
+		m2.put("foo", "bar");
+		m2.put("foo", "baz");
+		m2.put("qux", "quux");
+
+		String t3 = o.writerWithType(new TypeReference<TreeMultimap<String, String>>() {}).writeValueAsString(m2);
+		javaMap = o.readValue(t3, Map.class);
+		assertEquals(2, javaMap.size());
+
+		String t4 = o.writerWithType(new TypeReference<Multimap<String, String>>() {}).writeValueAsString(m2);
+		javaMap = o.readValue(t4, Map.class);
+		assertEquals(2, javaMap.size());
+	}
+
+	// Example from http://programmerbruce.blogspot.com/2011/05/deserialize-json-with-jackson-into.html
+	// and http://wiki.fasterxml.com/JacksonPolymorphicDeserialization
+
+	public void testTypeInfo() throws Exception {
+		final Zoo zoo = new Zoo();
+		final Dog dog = new Dog("Spike", "mutt", "red");
+		final Cat cat = new Cat("Fluffy", "spider ring");
+		final String dogJson = MAPPER.writeValueAsString(dog);
+		final Animal dogReincarnated = MAPPER.readValue(dogJson, Animal.class);
+		assertEquals(dog, dogReincarnated);
+		final String catJson = MAPPER.writeValueAsString(cat);
+		final Animal catReincarnated = MAPPER.readValue(catJson, Animal.class);
+		assertEquals(cat, catReincarnated);
+		zoo.animals = ImmutableList.of(dog, cat);
+		final String json1 = MAPPER.writeValueAsString(zoo);
+		System.err.println("zoo json1: "+json1);
+		final NameZoo nameZoo = new NameZoo();
+		nameZoo.nameToAnimals = ImmutableMultimap.of(dog.name, dog, cat.name, cat);
+//		nameZoo.nameToAnimals = ImmutableMap.of(dog.name, dog, cat.name, cat);
+		final String json2 = MAPPER.writeValueAsString(nameZoo);
+		System.err.println("nameZoo json2: "+json2);
+		final NameZoo nameZoo2 = MAPPER.readValue(json2, NameZoo.class);
+		// type info {- is -} was missing!
+		assertEquals(nameZoo, nameZoo2);
+	}
+
+	static class NameZoo {
+		NameZoo() {}
+		@JsonCreator
+		NameZoo(@JsonProperty("nameToAnimals") Multimap<String, Animal> nameToAnimals) {
+//		NameZoo(@JsonProperty("nameToAnimals") Map<String, Animal> nameToAnimals) {
+			this.nameToAnimals = nameToAnimals;
+		}
+		public Multimap<String, Animal> nameToAnimals;
+//		public Map<String, Animal> nameToAnimals;
+		@Override
+		public int hashCode() {
+			return Objects.hashCode(nameToAnimals);
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof NameZoo) {
+				final NameZoo that = (NameZoo) obj;
+				return this.nameToAnimals.equals(that.nameToAnimals);
+			}
+			return false;
+		}
+	}
+
+	static class Zoo {
+		public Collection<Animal> animals;
+	}
+
+	@JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
+	include = JsonTypeInfo.As.PROPERTY,
+	property = "type")
+	@JsonSubTypes({
+		@Type(value = Cat.class, name = "cat"),
+		@Type(value = Dog.class, name = "dog")})
+	static abstract class Animal {
+		public String name;
+	}
+
+	static class Dog extends Animal {
+		@JsonCreator
+		Dog(@JsonProperty("name") final String name,
+				@JsonProperty("breed") final String breed,
+				@JsonProperty("leashColor") final String leashColor) {
+			this.name = name;
+			this.breed = breed;
+			this.leashColor = leashColor;
+		}
+		public String breed;
+		public String leashColor;
+		@Override
+		public int hashCode() {
+			return Objects.hashCode(name, breed, leashColor);
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof Dog) {
+				Dog that = (Dog) obj;
+				return Objects.equal(this.name, that.name) &&
+					Objects.equal(this.breed, that.breed) &&
+					Objects.equal(this.leashColor, that.leashColor);
+			}
+			return false;
+		}
+	}
+
+	static class Cat extends Animal {
+		@JsonCreator
+		Cat(@JsonProperty("name") final String name,
+			@JsonProperty("favoriteToy") final String favoriteToy) {
+			this.name = name;
+			this.favoriteToy = favoriteToy;
+		}
+		public String favoriteToy;
+		@Override
+		public int hashCode() {
+			return Objects.hashCode(name, favoriteToy);
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof Cat) {
+				Cat that = (Cat) obj;
+				return Objects.equal(this.name, that.name) &&
+					Objects.equal(this.favoriteToy, that.favoriteToy);
+			}
+			return false;
+		}
+	}
 }
